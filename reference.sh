6fc2fbbe7a175b5ccd6b9e99df42b5c347f5afd0
Ugur <ugurbd@gmail.com>
2020-03-03 11:55:46 +0300

    Add files via upload

---
+++ reference.sh
@@ -0,0 +1,69 @@
+# This is a reference file for *nix utilities.
+
+# cat
+# sort
+# uniq
+# grep
+# wc
+# head
+# tail
+# tee
+
+# echo
+# type
+# which
+# whereis 
+# whatis
+# alias
+# printenv
+# apropos
+# man
+# info
+# help
+# alias
+
+# mv
+# cp
+# mkdir
+# rm
+# rmdir
+# ls
+# clear
+# history
+
+# clang
+# gcc
+# gdb
+# lldb
+# strace
+# ltrace
+# xxd
+# hd
+# hexdump
+# readelf
+# objdump
+# 
+
+# mc
+# tmux
+# htop
+
+# CTRL-a move cursor to the beginning of the line
+# CTRL-e move cursor to the end of the line
+# ALT-f move cursor forward one word
+# ALT-b move cursor backward one word
+# CTRL-l clear screen
+# CTRL-f cursor forward one character
+# CTRL-b cursor backward one character
+
+# CTRL-d
+# CTRL-t
+# ALT-t
+# ALT-l
+# ALT-u
+
+# CTRL-k
+# CTRLu
+# ALT-d
+# ALT-Backspace
+# CTRL-y
